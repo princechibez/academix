4744753a65ba977adc1d759fcb5b15d8bf2bc3bb
John Okorie <ameachijohn001@gmail.com>
2023-06-02 23:17:33 +0100

    page refactoring

--- academix/src/index.js
+++ academix/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
+import theme from './styles/theme';
 import LandingPage from './pages/landing/landingPage';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <LandingPage />
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <LandingPage/>
+      </CssBaseline>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
