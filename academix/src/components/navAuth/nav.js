7400087df9c801bf12914a7088c34f7766f2e95f
AnnyTheExplorer <agbataanastasia@gmail.com>
2023-06-03 14:15:15 +0100

    built landing page

---
+++ academix/src/components/navAuth/nav.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./nav.css";
+import logopurple from "../../assets/logopurple.png";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <div className="brand">
+        <span className="logo">
+          <img src={logopurple} alt="logo" width={35} />
+        </span>
+        <span className="brand-name">Academix</span>
+      </div>
+      <div className="search">
+        <input type="text" placeholder="Search" />
+      </div>
+      <div className="navigate">
+        <span className="categories">Categories</span>
+        <span className="about">About Us</span>
+      </div>
+      <div className="buttons">
+        <button className="signup">Sign Up</button>
+        <button className="signin">Sign In</button>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
--- academix/src/pages/landing/landingPage.js
+++ academix/src/pages/landing/landingPage.js
@@ -1,7 +1,26 @@
+import React from "react";
+import Nav from "../components/navAuth/nav.js";
+import Main from "../components/heroSection/main.js";
+import Range from "../components/range/range.js";
+import Topcourses from "../components/topCourses/topCourses.js";
+import Instructors from "../components/instructor/instructor.js";
+import Testimonials from "../components/testimonials/testimonials.js";
+import Levelup from "../components/levelUp/levelUp.js";
+import Newcourses from "../components/newCourses";
+import Footer from "../components/footer/footer.js";
+
 function LandingPage() {
   return (
     <div className="App">
-      <h1>ACADEMIX</h1>
+      <Nav />
+      <Main />
+      <Range />
+      <Topcourses />
+      <Instructors />
+      <Testimonials />
+      <Levelup />
+      <Newcourses />
+      <Footer />    
     </div>
   );
 }
