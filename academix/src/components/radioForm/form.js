65dce10946933723ce065e19cdad7ed0d22e0113
AnnyTheExplorer <agbataanastasia@gmail.com>
2023-06-04 10:10:18 +0100

    built joiningAs page

---
+++ academix/src/components/radioForm/form.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./form.css";
+import mainlogo from "../../assets/mainlogo.png";
+
+function Form() {
+  return (
+    <div className="form-container">
+      <div className="nav-logo">
+        <img src={mainlogo} alt="logo" width={80} />
+      </div>
+    <div className="form">
+      <div className="join">
+        <h1>Joining as?</h1>
+      </div>
+      <form>
+        <div className="student-option">
+          <span className="input">
+            <input type="radio" name="identity" value="student" />
+          </span>
+          <span className="student">A Student</span>
+        </div>
+        <div className="instructor-option">
+          <span className="input">
+            <input type="radio" name="identity" value="instructor" />
+          </span>
+          <span className="instructor">An Instructor</span>
+        </div>
+        <input type="button" value="Create Account" />
+        <p class="account-login">
+          Already have an account&#63;
+          <span className="signin-link"> Sign In</span>
+        </p>
+      </form>
+    </div>
+    </div>
+    
+  );
+}
+
+export default Form;
---
+++ academix/src/pages/joiningAs.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "../components/radioForm/form";
+
+function Joiningas() {
+  return (
+    <div className="joiningas" color="white">
+      <Form />
+    </div>
+  );
+}
+
+export default Joiningas;
