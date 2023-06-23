731f7200c5482611a6a6a69addc59a7640de2440
princechibez <chibezprince@gmail.com>
2023-06-23 07:42:49 -0700

    Added payment and optimised user classroom experience

--- academix/src/utility/axios.config.js
+++ academix/src/utility/axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://academix-api.onrender.com/api/v1",
   headers: {
     Authorization: localStorage.getItem("token"),
   },
